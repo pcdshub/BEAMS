b75a00e8cabd2cf10b6c5e5fb420f0d6f6fd2d60
josh-deb <joshc@slac.stanford.edu>
2024-02-14 19:37:43 -0800

    chore(SequencerState): some clean up

--- beams/sequencer/SequencerState.py
+++ beams/sequencer/SequencerState.py
@@ -3,11 +3,11 @@ from beams.sequencer.remote_calls.sequencer_pb2 import SequenceType, RunStateTyp
 from enum import Enum
 import logging
 
-SequencerStatesVariables = Enum('SequencerStatesVariables', 
-                                ["sequence", 
-                                 "node_name", 
-                                 "status",
-                                 "run_state"])
+SequencerStateVariable = Enum('SequencerStateVariable', 
+                              ["SEQUENCE", 
+                               "NODE_NAME", 
+                               "STATUS",
+                               "RUN_STATE"])
 
 
 class SequencerState():
@@ -16,11 +16,11 @@ class SequencerState():
   """
   def __init__(self):
     self.__lock__ = Lock()
-    self.state_dictionary = {i : 0 for i in SequencerStatesVariables}
-    self.state_dictionary[SequencerStatesVariables.sequence] = Value('i', SequenceType.NONE, lock=self.__lock__)
-    self.state_dictionary[SequencerStatesVariables.node_name] = Array('c', b'None', lock=self.__lock__)  # TODO: might want to wrap so we aren't dealing with byte strings 
-    self.state_dictionary[SequencerStatesVariables.status] = Value('i', TickStatus.UNKNOWN, lock=self.__lock__)
-    self.state_dictionary[SequencerStatesVariables.run_state] = Value('i', RunStateType.STATE_UNKNOWN, lock=self.__lock__)
+    self.state_dictionary = {i : 0 for i in SequencerStateVariable}
+    self.state_dictionary[SequencerStateVariable.SEQUENCE] = Value('i', SequenceType.NONE)
+    self.state_dictionary[SequencerStateVariable.NODE_NAME] = Array('c', b'None')  # TODO: wrap so we aren't dealing with byte strings 
+    self.state_dictionary[SequencerStateVariable.STATUS] = Value('i', TickStatus.UNKNOWN)
+    self.state_dictionary[SequencerStateVariable.RUN_STATE] = Value('i', RunStateType.STATE_UNKNOWN)
 
   def get_command_reply(self) -> dict:
     """
@@ -34,13 +34,16 @@ class SequencerState():
   def set_all_values(self, current_sequence: SequenceType, current_node: str, current_tick_status: TickStatus, current_run_state: RunStateType) -> None:
     logging.debug("Acquiring lock for 'set_all_values'")
     with self.__lock__:
-      self.state_dictionary[SequencerStatesVariables.current_sequence].value = current_sequence
-      self.state_dictionary[SequencerStatesVariables.current_node_name].value = current_node
-      self.state_dictionary[SequencerStatesVariables.current_status].value = current_tick_status
-      self.state_dictionary[SequencerStatesVariables.current_run_state].value = current_run_state
+      self.state_dictionary[SequencerStateVariable.current_sequence].value = current_sequence
+      self.state_dictionary[SequencerStateVariable.current_node_name].value = current_node
+      self.state_dictionary[SequencerStateVariable.current_status].value = current_tick_status
+      self.state_dictionary[SequencerStateVariable.current_run_state].value = current_run_state
 
-  def set_value(self, state: SequencerStatesVariables, value):
-    self.state_dictionary[state].value = value  # Todo(josh): wrap in try catch with log.
+  def set_value(self, state: SequencerStateVariable, value):
+    with self.state_dictionary[state].get_lock():
+      self.state_dictionary[state].value = value  # Todo(josh): wrap in try catch with log.
 
-  def get_value(self, state: SequencerStatesVariables):
-    return self.state_dictionary[state].value  # Todo(josh): wrap in try catch with log.
+  def get_value(self, state: SequencerStateVariable):
+    with self.state_dictionary[state].get_lock():
+      val = self.state_dictionary[state].value
+    return val  # Todo(josh): wrap in try catch with log.

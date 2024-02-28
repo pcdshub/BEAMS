9d64a8471c7e27f8a1d702dec96f7a51ac13706f
josh-deb <joshc@slac.stanford.edu>
2024-02-28 13:38:16 -0800

    fix(PriorityQueue): way better to do with the queue.PriorityQueue object...

--- beams/sequencer/helpers/PriorityQueue.py
+++ beams/sequencer/helpers/PriorityQueue.py
@@ -4,13 +4,14 @@ Warning this is not effecient to search, it is a dumb priority queue. If perform
 But also make it a binary tree, you could utilize the heapq library but it kind of sucks
 """
 
-import heapq
+from queue import PriorityQueue as pqueue
 from multiprocessing import Lock
+import os
 
 
 class PriorityQueue:
   def __init__(self, priority_dict):
-    self.__queue__ = []
+    self.__queue__ = pqueue(maxsize=100)
     self.__priority_dict__ = priority_dict
     self.__lock__ = Lock()
     self.entry_count = 0
@@ -22,11 +23,21 @@ class PriorityQueue:
       raise KeyError(f"Priority Enum provided {prio_enum} not in priority_dict")
 
   def put(self, ent, prio_enum):
+    print(f"acquiring the put lock on {os.getpid()}")
     with self.__lock__:
-      heapq.heappush(self.__queue__, (self.get_priority_int(prio_enum), 
-                                      self.entry_count, 
-                                      ent))
+      print("putting it in")
+      self.__queue__.put((self.get_priority_int(prio_enum), 
+                          self.entry_count,
+                          ent))
+      print(self.__queue__)
       self.entry_count += 1
+    print("lock released")
 
   def pop(self):
-    return heapq.heappop(self.__queue__)[-1]
+    print(f"acquiring the lock for pop on {os.getpid()}")
+    with self.__lock__:
+      print(self.__queue__)
+      val = self.__queue__.get()
+      print(val)
+      return val[-1]
+    print("releasing lock for pop")

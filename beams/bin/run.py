17898a755a4087bf848567867263a49f04e95fa8
tangkong <tangkongrobert@gmail.com>
2024-09-16 13:24:41 -0700

    WIP/ENH: fill out only a little more of the run subcommand

--- beams/bin/run.py
+++ beams/bin/run.py
@@ -6,6 +6,9 @@ from __future__ import annotations
 
 import argparse
 import logging
+from pathlib import Path
+
+from beams.tree_config import get_tree_from_path
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +32,15 @@ def build_arg_parser(argparser=None):
 
 def main(filepath: str):
     logger.info(f"Running behavior tree at {filepath}")
+    # grab config
+    fp = Path(filepath).resolve()
+    if not fp.is_file():
+        raise ValueError("Provided filepath is not a file")
+
+    tree = get_tree_from_path(fp)
+    print(tree)
+    # TODO: the rest of whatever we determine the "run" process to be
+    # run external server?
+    # setup tree?
+    # tick?
+    # settings for ticking? (continuous tick?, as separate process?)

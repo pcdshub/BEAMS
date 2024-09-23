893f6192cfbae66cd7ea94f809ff52cd61850a7c
tangkong <tangkongrobert@gmail.com>
2024-09-23 16:27:29 -0700

    TST: add basic cli tests

--- beams/tests/conftest.py
+++ beams/tests/conftest.py
@@ -1,4 +1,7 @@
 import logging
+import sys
+from contextlib import contextmanager
+from copy import copy
 
 import py_trees.logging
 import pytest
@@ -16,3 +19,26 @@ def central_logging_setup(caplog):
     setup_logging(logging.DEBUG)
     # Set py_trees logging level (not our library)
     py_trees.logging.level = py_trees.logging.Level.DEBUG
+
+
+@contextmanager
+def cli_args(args):
+    """
+    Context manager for running a block of code with a specific set of
+    command-line arguments.
+    """
+    prev_args = sys.argv
+    sys.argv = args
+    yield
+    sys.argv = prev_args
+
+
+@contextmanager
+def restore_logging():
+    """
+    Context manager for reverting our logging config after testing a function
+    that configures the logging.
+    """
+    prev_handlers = copy(logging.root.handlers)
+    yield
+    logging.root.handlers = prev_handlers

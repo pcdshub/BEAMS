e77777dbac897018bb5f8ff2916bb3d1e1c4f60c
Zachary Lentz <zlentz@slac.stanford.edu>
2024-10-02 16:04:28 -0700

    WIP: build out most of the rest of the dg1 part of the mfx tree

--- mfx_tree.py
+++ mfx_tree.py
@@ -1,4 +1,4 @@
-from beams.tree_config import CheckAndDoItem, ConditionItem, SetPVActionItem, ConditionOperator, SequenceItem, SequenceConditionItem, RangeConditionItem
+from beams.tree_config import CheckAndDoItem, ConditionItem, SetPVActionItem, ConditionOperator, SequenceItem, SequenceConditionItem, RangeConditionItem, SelectorItem
 
 # DG2 Stopper: remove
 check_dg2_stp_closed = ConditionItem(
@@ -149,3 +149,150 @@ check_mfx_att_range = RangeConditionItem(
     low_value=0.08,
     high_value=0.12,
 )
+check_calc_ready = ConditionItem(
+    name="check_calc_ready",
+    description="Wait for previous or current calc to be done",
+    pv="MFX:ATT:COM:CALCP",
+    value=0,
+)
+act_prepare_transmission = SetPVActionItem(
+    name="act_prepare_transmission",
+    description="Set the transmission that will be applied",
+    pv="MFX:ATT:COM:R_DES",
+    value=0.1,
+)
+act_apply_transmission = SetPVActionItem(
+    name="act_apply_transmission",
+    description="Move the attenuator to the lesser of the two att options",
+    pv="MFX:ATT:COM:GO",
+    value=3,
+    termination_check=check_mfx_att_range,
+)
+seq_set_mfx_att = SequenceItem(
+    name="seq_set_mfx_att",
+    description="Set the mfx att to 10% transmission",
+    children=[check_calc_ready, act_prepare_transmission, act_apply_transmission],
+)
+mfx_att = SelectorItem(
+    name="mfx_att",
+    description="Ensure mfx att is near 10% transmission for alignment",
+    children=[check_mfx_att_range, seq_set_mfx_att],
+)
+
+# Slits: all to 2mm
+slit_cads = []
+for slit_name, pv_ext in (
+    ("dg1", "DG1:JAWS"),
+    ("dg2_us", "DG2:JAWS:US"),
+    ("dg2_ms", "DG2:JAWS:MS"),
+    ("dg2_ds", "DG2:JAWS:DS"),
+):
+    base_pv = f"MFX:{pv_ext}:"
+    for axis in ("X", "Y"):
+        axis_name = f"{slit_name}_{axis.lower()}"
+        slit_check = RangeConditionItem(
+            name=f"{axis_name}_check",
+            description="Check if slit width is close to 2mm",
+            pv=f"{base_pv}Actual_{axis}WIDTH",
+            low_value=1.9,
+            high_value=2.1,
+        )
+        slit_move = SetPVActionItem(
+            name=f"{axis_name}_move",
+            description="Move slit width to 2mm",
+            pv=f"{base_pv}{axis}WID_REQ",
+            value=2,
+        )
+        slcad = CheckAndDoItem(
+            name=f"{axis_name}_check_and_do",
+            description="Ensure slit width is close to 2mm",
+            check=slit_check,
+            do=slit_move,
+        )
+        slit_cads.append(slcad)
+slits_to_2mm = SequenceItem(
+    name="slits_to_2mm",
+    description="Ensure all slit widths are close to 2mm",
+    children=slit_cads,
+)
+
+# MR1L4 MFX/MEC mirror
+check_mr1l4_xstate = ConditionItem(
+    name="check_mr1l4_xstate",
+    description="Check if mr1l4 is inserted",
+    pv="MR1L4:HOMS:MMS:XUP:STATE:GET_RBV",
+    value="IN",
+    operator=ConditionOperator.equal,
+)
+act_mr1l4_xstate = SetPVActionItem(
+    name="act_mr1l4_xstate",
+    description="Move mr1l4 in",
+    pv="MR1L4:HOMS:MMS:XUP:STATE:SET",
+    value="IN",
+)
+insert_mr1l4 = CheckAndDoItem(
+    name="insert_mr1l4",
+    description="Ensure mr1l4 is in",
+    check=check_mr1l4_xstate,
+    do=act_mr1l4_xstate,
+)
+mr1l4_nominal = -544
+check_mr1l4_pointing = RangeConditionItem(
+    name="check_mr1l4_pointing",
+    description="Check if mr1l4 is pointing generally towards MFX",
+    pv="MR1L4:HOMS:MMS:PITCH.RBV",
+    low_value=mr1l4_nominal-5,
+    high_value=mr1l4_nominal+5,
+)
+act_mr1l4_pitch = SetPVActionItem(
+    name="act_mr1l4_pitch",
+    description="Move mr1l4 back to the nominal pitch",
+    pv="MR1L4:HOMS:MMS:PITCH.VAL",
+    value=mr1l4_nominal,
+)
+point_mr1l4 = CheckAndDoItem(
+    name="point_mr1l4",
+    description="Ensure mr1l4 is pointed towards MFX",
+    check=check_mr1l4_pointing,
+    do=act_mr1l4_pitch,
+)
+prepare_mr1l4 = SequenceItem(
+    name="prepare_mr1l4",
+    description="Get mr1l4 ready for MFX beam",
+    children=[insert_mr1l4, point_mr1l4],
+)
+
+# Set up the DG1 camera
+check_dg1_cam_running = ConditionItem(
+    name="Check_dg1_cam_running",
+    description="Check if the camera is acquiring frames",
+    pv="MFX:DG1:P6740:ArrayRate_RBV",
+    value=1,
+    operator=ConditionOperator.greater_equal,
+)
+# If the cam isn't running, it either needs to be turned on, or off and back on
+# Simplest is to put the "off" and then the "on" in series without checking.
+act_dg1_no_acq = SetPVActionItem(
+    name="act_dg1_no_acq",
+    description="Stop acquiring",
+    pv="MFX:DG1:P6740:Acquire",
+    value=0,
+)
+act_dg1_yes_acq = SetPVActionItem(
+    name="act_dg1_yes_acq",
+    description="Start acquiring",
+    pv="MFX:DG1:P6740:Acquire",
+    value=1,
+    termination_check=check_dg1_cam_running,
+)
+seq_dg1_acq_cycle = SequenceItem(
+    name="seq_dg1_acq_cycle",
+    description="Stop and then start the acquisition",
+    children=[act_dg1_no_acq, act_dg1_yes_acq],
+)
+ensure_dg1_cam_running = SelectorItem(
+    name="ensure_dg1_cam_running",
+    description="Refresh acquisition if needed",
+    children=[check_dg1_cam_running, seq_dg1_acq_cycle],
+)
+# Skip stats for now

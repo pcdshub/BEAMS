d5737208461a921a0f5a0c698022205df32c60b1
josh-deb <joshc@slac.stanford.edu>
2024-10-16 13:09:19 -0700

    chore: aggregated custom types in new file typing_helper. hopefully this adds clarity. clean up of some Worker debug messages, fixed wrong type hint, made compatible with py 3.10 and 3.9

--- beams/tree_config.py
+++ beams/tree_config.py
@@ -3,11 +3,10 @@ from __future__ import annotations
 import json
 import logging
 import operator
-import time
 from dataclasses import dataclass, field, fields
 from enum import Enum
 from pathlib import Path
-from typing import Any, Callable, List, Optional, Union
+from typing import Any, List, Optional, Union
 
 import py_trees
 from apischema import deserialize, serialize
@@ -18,6 +17,7 @@ from py_trees.behaviours import (CheckBlackboardVariableValue,
 from py_trees.common import ComparisonExpression, ParallelPolicy, Status
 from py_trees.composites import Parallel, Selector, Sequence
 
+from beams.typing_helper import Evaluatable
 from beams.behavior_tree.ActionNode import ActionNode, wrapped_action_work
 from beams.behavior_tree.CheckAndDo import CheckAndDo
 from beams.behavior_tree.ConditionNode import ConditionNode
@@ -165,7 +165,7 @@ class ConditionItem(BaseItem):
         cond_func = self.get_condition_function()
         return ConditionNode(self.name, cond_func)
 
-    def get_condition_function(self) -> Callable[[], bool]:
+    def get_condition_function(self) -> Evaluatable:
         op = getattr(operator, self.operator.value)
 
         def cond_func():
@@ -191,7 +191,7 @@ class SetPVActionItem(BaseItem):
     def get_tree(self) -> ActionNode:
 
         @wrapped_action_work(self.loop_period_sec)
-        def work_func(comp_condition: Callable[[], bool]):
+        def work_func(comp_condition: Evaluatable) -> py_trees.common.Status:
             try:
                 # Set to running
                 value = caget(self.pv)  # double caget, this
@@ -229,7 +229,7 @@ class IncPVActionItem(BaseItem):
     def get_tree(self) -> ActionNode:
 
         @wrapped_action_work(self.loop_period_sec)
-        def work_func(comp_condition: Callable[[], bool]) -> py_trees.common.Status:
+        def work_func(comp_condition: Evaluatable) -> py_trees.common.Status:
             """
             To be run inside of a while loop
             Action node should take care of logging, reporting status

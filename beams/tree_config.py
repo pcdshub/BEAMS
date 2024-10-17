f9b654874e638470b22450233a65100aebdf4def
Zachary Lentz <zlentz@slac.stanford.edu>
2024-10-17 16:59:55 -0700

    DOC/MNT: rename new function for clarity and expand docstrings

--- beams/tree_config.py
+++ beams/tree_config.py
@@ -27,7 +27,12 @@ logger = logging.getLogger(__name__)
 
 
 def get_tree_from_path(path: Path) -> py_trees.trees.BehaviourTree:
-    """Deserialize a json file, return the tree it specifies"""
+    """
+    Deserialize a json file, return the tree it specifies.
+
+    This can be used internally to conveniently and consistently load
+    serialized json files as ready-to-run behavior trees.
+    """
     with open(path, "r") as fd:
         deser = json.load(fd)
         tree_item = deserialize(BehaviorTreeItem, deser)
@@ -35,8 +40,17 @@ def get_tree_from_path(path: Path) -> py_trees.trees.BehaviourTree:
     return tree_item.get_tree()
 
 
-def save_tree_to_path(path: Union[Path, str], root: BaseItem):
-    """Serialize a behavior tree node to a json file."""
+def save_tree_item_to_path(path: Union[Path, str], root: BaseItem):
+    """
+    Serialize a behavior tree item to a json file.
+
+    This can be used to generate serialized trees from python scripts.
+    The user needs to create various interwoven tree items, pick the
+    correct item to be the root node, and then use this function to
+    save the serialized file.
+
+    These files are ready to be consumed by get_tree_from_path.
+    """
     ser = serialize(BehaviorTreeItem(root=root))
 
     with open(path, "w") as fd:

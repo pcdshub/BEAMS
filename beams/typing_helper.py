8716fb59e3e381d624dd042654f4bb97e252dd42
josh-deb <joshc@slac.stanford.edu>
2024-10-18 15:13:18 -0700

    good nits

--- beams/typing_helper.py
+++ beams/typing_helper.py
@@ -5,7 +5,7 @@ Hint at rational behind function handles as entrypoint for debugging / learning
 """
 
 
-from typing import Callable, Any
+from typing import Callable
 from multiprocessing import Queue, Event, Value
 
 import py_trees
@@ -16,16 +16,16 @@ from beams.behavior_tree.VolatileStatus import VolatileStatus
 '''
 Evaluatable: function handle that returns a boolean , accpeting arbitrary arugments 
 '''
-Evaluatable = Callable[[Any], bool]
+Evaluatable = Callable[..., bool]
 
 '''
-Defines signature for an ActioNode work loop function handle.
+Defines signature for an ActionNode work loop function handle.
 This is neccisated by how beams.behavior_tree.ActionWoker expects to spawn the work process via base class beams.sequencer.helpers.Worker
 Parameter Types: 
   Value: volatile indicating work remains to be performed (tree is still ticking, program still running)
   str: name of process
   Event: work gate reflecting py_trees state of node with respect to tree
-  VolatileState: mechanism to signal from this process to other processes what the py_trees.common.Status of this node is
+  VolatileStatus: mechanism to signal from this process to other processes what the py_trees.common.Status of this node is
   Evaluatable: this is a completion condition that allows exit of the while loop within the work loop which will wait for a py_trees "initialise"
     call to reset the Event (work gate) such that meaningful work can resume on this process
   Queue: logging queue 

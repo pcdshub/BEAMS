370a08afd146d1954e607d807f3111b99ae37a74
Zachary Lentz <zlentz@slac.stanford.edu>
2024-11-01 14:20:00 -0700

    TST: add some tests for specific tree config subitems

--- beams/tests/test_tree_config.py
+++ beams/tests/test_tree_config.py
@@ -1,3 +1,5 @@
+from itertools import combinations_with_replacement
+
 import apischema
 import pytest
 from py_trees.behaviour import Behaviour
@@ -14,8 +16,9 @@ from py_trees.composites import Parallel, Selector, Sequence
 from beams.behavior_tree.ActionNode import ActionNode
 from beams.behavior_tree.CheckAndDo import CheckAndDo
 from beams.behavior_tree.ConditionNode import ConditionNode
-from beams.tree_config import (BaseItem, BlackboardToStatusItem,
-                               CheckAndDoItem,
+from beams.serialization import get_all_subclasses
+from beams.tree_config import (BaseConditionItem, BaseItem,
+                               BlackboardToStatusItem, CheckAndDoItem,
                                CheckBlackboardVariableExistsItem,
                                CheckBlackboardVariableValueItem, ConditionItem,
                                DummyItem, FailureItem, IncPVActionItem,
@@ -55,17 +58,65 @@ ITEM_TO_BEHAVIOUR = [
 ]
 
 
-@pytest.mark.parametrize('item, node_type', ITEM_TO_BEHAVIOUR)
-def test_get_tree(item: BaseItem, node_type: Behaviour):
-    item_instance = item()
+@pytest.mark.parametrize('item_class, node_type', ITEM_TO_BEHAVIOUR)
+def test_get_tree(item_class: type[BaseItem], node_type: type[Behaviour]):
+    item_instance = item_class()
     assert isinstance(item_instance.get_tree(), node_type)
 
 
 @pytest.mark.parametrize('item_class', [item[0] for item in ITEM_TO_BEHAVIOUR])
-def test_item_serialize_roundtrip(item_class: BaseItem):
+def test_item_serialize_roundtrip(item_class: type[BaseItem]):
     item = item_class()
     ser = apischema.serialize(item_class, item)
 
     deser = apischema.deserialize(item_class, ser)
 
     assert item == deser
+
+
+@pytest.mark.parametrize(
+    'item_class, attr, expand',
+    [
+        (SetPVActionItem, "termination_check", BaseConditionItem),
+        (IncPVActionItem, "termination_check", BaseConditionItem),
+        (CheckAndDoItem, "check", BaseConditionItem),
+    ]
+)
+def test_item_serialize_roundtrip_union_singles(item_class: type[BaseItem], attr: str, expand: type[BaseItem]):
+    count = 0
+    for cls in get_all_subclasses(expand):
+        if cls.__name__.startswith("Base"):
+            continue
+        item = item_class(**{attr: cls()})
+        ser = apischema.serialize(item_class, item)
+        deser = apischema.deserialize(item_class, ser)
+        assert item == deser
+        count += 1
+    # If all subclasses skip assert item == deser, we should also have an error
+    assert count > 0
+
+
+@pytest.mark.parametrize(
+    'item_class, attr, expand',
+    [
+        (ParallelItem, "children", BaseItem),
+        (SelectorItem, "children", BaseItem),
+        (SequenceItem, "children", BaseItem),
+        (SequenceConditionItem, "children", BaseConditionItem),
+    ]
+)
+def test_item_serailize_roundtrip_union_sublists(item_class: type[BaseItem], attr: str, expand: type[BaseItem]):
+    options = [cls for cls in get_all_subclasses(expand) if not cls.__name__.startswith("Base")]
+    combinations = []
+    for size in (1, 2):
+        for variant in combinations_with_replacement(options, size):
+            combinations.append(variant)
+    count = 0
+    for tuple_of_cls in combinations:
+        item = item_class(**{attr: [cls() for cls in tuple_of_cls]})
+        ser = apischema.serialize(item_class, item)
+        deser = apischema.deserialize(item_class, ser)
+        assert item == deser
+        count += 1
+    # If all subclasses skip assert item == deser, we should also have an error
+    assert count > 0

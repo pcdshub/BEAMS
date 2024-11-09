4af2f95f6c59989a6e406d95eba181ac864f200e
Zachary Lentz <zlentz@slac.stanford.edu>
2024-11-08 16:31:47 -0800

    ENH: make script more able to let you use your own python env

--- examples/mfx_tree_test.sh
+++ examples/mfx_tree_test.sh
@@ -2,14 +2,17 @@
 here="$(realpath "$(dirname ${BASH_SOURCE[0]})")"
 cd "${here}"
 
-if [ -z "${CONDA_PREFiX}" ]; then
+# If you don't have a conda env or a venv, let's use this one
+if [[ -z "${CONDA_PREFiX}" ]] && [[ -z "${VIRTUAL_ENV}" ]]; then
   conda_bin="/cds/group/pcds/pyps/conda/py39/envs/pcds-5.9.1/bin/"
-else
-  # Make sure your env has pydm and caproto!
-  conda_bin="${CONDA_PREFIX}/bin"
+  export PATH="${conda_bin}:${PATH}"
 fi
-export PATH="${conda_bin}:${PATH}"
-export PYTHONPATH="$(realpath "${here}"/..)"
+# If you didn't set up your python path, let's use this clone of beams
+if [[ -z "${PYTHONPATH}" ]]; then
+  export PYTHONPATH="$(realpath "${here}"/..)"
+fi
+
+# Align with sim IOC, do not touch real devices on 5068!
 export EPICS_CA_SERVER_PORT=5066
 
 python mfx_tree.py

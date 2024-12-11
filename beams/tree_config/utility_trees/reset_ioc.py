87346806fe3c5bab3b6a1fe91b235e6f2fb7ccc8
josh-deb <joshc@slac.stanford.edu>
2024-12-11 13:09:01 -0800

    enh: resetting feature flag of heartbeat count on successful reset so if this node gets triggered twice itll recache heartbeat

--- beams/tree_config/utility_trees/reset_ioc.py
+++ beams/tree_config/utility_trees/reset_ioc.py
@@ -25,12 +25,12 @@ class ResetIOCItem(BaseItem):
 
     def __post_init__(self):
         # non dataclass PVss
-        self.hbeat_val = ProcessIntValue(value=-1)  # set to unachievable heartbeat val 
+        self.hbeat_val = ProcessIntValue(value=-1)  # set to unachievable heartbeat val
         self.name = f"{self.ioc_prefix}_reset_tree"
 
     def get_tree(self) -> Sequence:
         def check_acquired_current_hbeat():
-            val = self.hbeat_val.get_value() != -1  # set to unachievable heartbeat val 
+            val = self.hbeat_val.get_value() != -1  # set to unachievable heartbeat val
             logger.debug(f"Heartbeat cached as {val}, {self.hbeat_val.get_value()}")
             return val
 
@@ -59,7 +59,18 @@ class ResetIOCItem(BaseItem):
                                      loop_period_sec=0.1,  # this is greater than work_timeout period, should only happen once.
                                      termination_check=reset_success_termination_condiiton)
 
+        # get the current heartbeat of IOC
+        @wrapped_action_work(loop_period_sec=0.1)
+        def reset_cache_hbeat_wfunc(comp_condition: Evaluatable) -> py_trees.common.Status:
+            self.hbeat_val.set_value(-1)
+            logger.debug(f"<<-- Resetting cached ioc hbeat for tree: {self.ioc_prefix}")
+
+            return py_trees.common.Status.SUCCESS
+        reset_heartbeat_cache = ActionNode(name=f"{self.ioc_prefix}_reset_heartbeat_cache",
+                                           work_func=reset_cache_hbeat_wfunc,
+                                           completion_condition=lambda : self.hbeat_val.get_value() == -1)
+
         root = Sequence(name=self.name,
                         memory=True,
-                        children=[cache_current_heartbeat, send_reset.get_tree()])
+                        children=[cache_current_heartbeat, send_reset.get_tree(), reset_heartbeat_cache])
         return root

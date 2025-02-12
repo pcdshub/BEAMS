b955157954a1ae1855479b056b0af4a306957523
josh-deb <joshc@slac.stanford.edu>
2025-02-11 17:14:28 -0800

    feat: implemented tree cleaning for beams service, calls stop work on the work thread of tree ticker & calls shutdown on the tree

--- beams/bin/beams_service.py
+++ beams/bin/beams_service.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import sys
-import os
-=======
-# toss arg parse here to start
->>>>>>> d0aa23b (feat: ticking through trees loaded in memory!)
 import logging
 import os
 import sys
@@ -14,7 +8,7 @@ from beams.logging import setup_logging
 from beams.service.helpers.worker import Worker
 from beams.service.remote_calls.command_pb2 import CommandType
 from beams.service.rpc_handler import RPCHandler
-from beams.service.tree_ticker import TreeTicker, TreeState
+from beams.service.tree_ticker import TreeState, TreeTicker
 
 logger = logging.getLogger(__name__)
 
@@ -24,7 +18,7 @@ class BeamsService(Worker):
         # TODO: make a singleton. Make process safe by leaving artifact file
         super().__init__("BeamsService", grace_window_before_terminate_seconds=0.5)
 
-         # remote manager: https://docs.python.org/3/library/multiprocessing.html#using-a-remote-manager
+        # remote manager: https://docs.python.org/3/library/multiprocessing.html#using-a-remote-manager
         class sync_man(BaseManager):
           pass
 
@@ -36,6 +30,14 @@ class BeamsService(Worker):
         self.sync_man = sync_man()
         self.sync_man.start()
         logger.debug(f"Sync Man starting at: {self.sync_man.address}")
+
+    def join_all_trees(self):
+        with self.sync_man as man:
+          tree_dict = man.get_tree_dict()
+          for tree_name, tree in tree_dict.items():
+              print(f"Cleaning up tree of name {tree_name}")
+              tree.stop_work()
+              tree.shutdown()
 
     def work_func(self):
         self.grpc_service = RPCHandler(sync_manager=self.sync_man)
@@ -50,32 +52,43 @@ class BeamsService(Worker):
                     logger.debug(f"inbound command {request}")
 
                     if (request.command_t == CommandType.LOAD_NEW_TREE):
-                        logger.debug(f"ayo no in man we're at: {os.getpid()}")
                         with self.sync_man as man:
-                          tick_config_mess = request.load_new_tree.tick_spec
-                          init_state = man.TreeState(
-                            tick_delay_ms=tick_config_mess.delay_ms,
-                            tick_config=tick_config_mess.tick_config
-                          )
-                          x = man.TreeTicker(filepath=request.load_new_tree.tree_file_path,
-                                             init_tree_state=init_state)
-                          tree_dict = man.get_tree_dict()
-                          tree_dict.update({request.tree_name : x})
-                          logger.debug(f"Loaded tree of name {request.tree_name}" +
-                                       f" from filepath: {request.load_new_tree.tree_file_path}" +
-                                       " explciit START_TREE command will need to be issued for it to begin")
+                            tick_config_mess = request.load_new_tree.tick_spec
+                            init_state = man.TreeState(
+                                tick_delay_ms=tick_config_mess.delay_ms,
+                                tick_config=tick_config_mess.tick_config
+                            )
+                            x = man.TreeTicker(filepath=request.load_new_tree.tree_file_path,
+                                               init_tree_state=init_state)
+                            tree_dict = man.get_tree_dict()
+                            tree_dict.update({request.tree_name : x})
+                            logger.debug(f"Loaded tree of name {request.tree_name}" +
+                                         f" from filepath: {request.load_new_tree.tree_file_path}" +
+                                         " explciit START_TREE command will need to be issued for it to begin")
                     elif (request.command_t == CommandType.START_TREE):
-                      with self.sync_man as man:
-                        # get tree, again for now this is tree specified in json file,
-                        # disambugiate this later
-                        tree_name = request.tree_name
-                        # get tree
-                        tree_dict = man.get_tree_dict()
-                        if (tree_name not in tree_dict.keys()):
-                          logging.error(f"Sorry fam {tree_name} is not in tree_dictionary: {tree_dict}")
-                          continue
-                        tree_to_start = tree_dict.get(tree_name)
-                        tree_to_start.start_tree()
+                        with self.sync_man as man:
+                            # get tree, again for now this is tree specified in json file,
+                            # disambugiate this later
+                            tree_name = request.tree_name
+                            # get tree
+                            tree_dict = man.get_tree_dict()
+                            if (tree_name not in tree_dict.keys()):
+                                logging.error(f"{tree_name} is not in tree_dictionary: {tree_dict}")
+                                continue
+                            tree_to_start = tree_dict.get(tree_name)
+                            tree_to_start.start_tree()
+                    elif (request.command_t == CommandType.PAUSE_TREE):
+                        with self.sync_man as man:
+                            # get tree, again for now this is tree specified in json file,
+                            # disambugiate this later
+                            tree_name = request.tree_name
+                            # get tree
+                            tree_dict = man.get_tree_dict()
+                            if (tree_name not in tree_dict.keys()):
+                                logging.error(f"{tree_name} is not in tree_dictionary: {tree_dict}")
+                                continue
+                            tree_to_start = tree_dict.get(tree_name)
+                            tree_to_start.pause_tree()
 
             except Exception as e:
                 e_type, e_object, e_traceback = sys.exc_info()
@@ -104,6 +117,7 @@ def main(*args, **kwargs):
 
     while (input("press q to kill") != 'q'):
       time.sleep(1)
+    x.join_all_trees()
     x.stop_work()
 
 

bfec43c58a00e98efbcc33a3adfa2996762ad0be
tangkong <tangkongrobert@gmail.com>
2025-04-11 14:21:17 -0700

    STY: fix style sins

--- beams/bin/service_main.py
+++ beams/bin/service_main.py
@@ -19,7 +19,7 @@ class BeamsService(Worker):
 
         # remote manager: https://docs.python.org/3/library/multiprocessing.html#using-a-remote-manager
         class SyncMan(BaseManager):
-          pass
+            pass
 
         self.tree_dict = {}
         SyncMan.register("TreeTicker", TreeTicker)
@@ -55,7 +55,8 @@ class BeamsService(Worker):
         # the job of this work function will be to consume messages and update the managed trees
         while (self.do_work.value):
             try:
-                if self.grpc_service.command_ready_sem.acquire(timeout=0.2):  # block untill we get something to work on
+                # block untill we get something to work on
+                if self.grpc_service.command_ready_sem.acquire(timeout=0.2):
                     request = self.grpc_service.incoming_command_queue.get()
                     logger.debug(f"inbound command {request}")
 
@@ -83,9 +84,9 @@ class BeamsService(Worker):
                                init_tree_state=init_state)
             tree_dict = man.get_tree_dict()
             tree_dict.update({request.tree_name : x})
-            logger.debug(f"Loaded tree of name {request.tree_name}" +
-                         f" from filepath: {request.load_new_tree.tree_file_path}" +
-                         " explciit START_TREE command will need to be issued for it to begin")
+            logger.debug(f"Loaded tree ({request.tree_name}) from filepath: "
+                         f"{request.load_new_tree.tree_file_path}.  Explcit "
+                         "START_TREE command needed to begin ticking.")
 
     def start_tree(self, request: CommandMessage) -> None:
         tree_name = request.tree_name
@@ -120,13 +121,13 @@ class BeamsService(Worker):
 
 
 def main(*args, **kwargs):
-    x = BeamsService()
-    x.start_work()
+    service = BeamsService()
+    service.start_work()
 
     while (input("press q to kill") != 'q'):
         time.sleep(1)
-    x.join_all_trees()
-    x.stop_work()
+    service.join_all_trees()
+    service.stop_work()
 
 
 if __name__ == "__main__":

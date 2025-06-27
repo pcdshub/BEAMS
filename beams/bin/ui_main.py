9aa602f64e71c7637a21425e6124cfb0db057fda
tangkong <tangkongrobert@gmail.com>
2025-06-27 11:29:57 -0700

    ENH/WIP: first pass at beams ui tree builder cli entrypoint

---
+++ beams/bin/ui_main.py
@@ -0,0 +1,24 @@
+import qtpynodeeditor as nodeeditor
+from qtpy.QtWidgets import QApplication
+from qtpynodeeditor.node_geometry import LayoutDirection
+
+from beams.widgets.models import RootNodeModel, add_items_to_registry
+
+
+def main(*args, **kwargs):
+    app = QApplication([])
+    registry = nodeeditor.DataModelRegistry()
+
+    my_style = nodeeditor.StyleCollection()
+    my_style.node.layout_direction = LayoutDirection.VERTICAL
+
+    add_items_to_registry(registry, style=my_style)
+    registry.register_model(RootNodeModel, style=my_style)
+    scene = nodeeditor.FlowScene(style=my_style, registry=registry)
+
+    view = nodeeditor.FlowView(scene)
+    view.setWindowTitle("Beams Behavior Tree Builder")
+    view.resize(800, 600)
+    view.show()
+
+    app.exec_()

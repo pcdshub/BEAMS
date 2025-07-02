e20708dabc7dc6b88d57420118187d26b2c2b056
Zachary Lentz <zlentz@slac.stanford.edu>
2025-07-02 14:32:21 -0700

    ENH: use the normal venv dir, get the server launching

--- beams_service_files/entry_point.sh
+++ beams_service_files/entry_point.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
 THIS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-cd "$THIS_DIR"
+cd "$THIS_DIR/.."
 source venv/bin/activate
-python3 -m beams.bin.beams_service "$@"
+python3 -m beams service "$@"
--- beams_service_files/install_beams.sh
+++ beams_service_files/install_beams.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 set -e
 THIS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-cd "$THIS_DIR"
-python3 -m venv venv
-source venv/bin/activate
-pip3 install $THIS_DIR/..
+cd "$THIS_DIR/.."
+if [ -d venv ]; then
+    source venv/bin/activate
+else
+    python3 -m venv venv
+    source venv/bin/activate
+    pip3 install .
+fi
+make gen_grpc

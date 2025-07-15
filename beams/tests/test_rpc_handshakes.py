0a6b818275f1da645ee3ba1e3614563c676fbf2c
tangkong <tangkongrobert@gmail.com>
2025-07-15 15:57:54 -0700

    TST: add tests for tree details and TreeIdKey, adjust tests to reflect fact that trees are identified by given name, not name stored in config file

--- beams/tests/test_rpc_handshakes.py
+++ beams/tests/test_rpc_handshakes.py
@@ -1,7 +1,9 @@
 from functools import partial
 from pathlib import Path
 
-from beams.service.remote_calls.behavior_tree_pb2 import TickStatus, TreeStatus
+from beams.service.remote_calls.behavior_tree_pb2 import (TickStatus,
+                                                          TreeDetails,
+                                                          TreeStatus)
 from beams.service.remote_calls.generic_message_pb2 import MessageType
 from beams.service.remote_calls.heartbeat_pb2 import HeartBeatReply
 from beams.service.rpc_client import RPCClient
@@ -56,11 +58,11 @@ def test_load_run_continuous_tree(rpc_client: RPCClient):
     wait_until(partial(assert_heartbeat_has_n_trees, rpc_client, 1))
     # tree name taken from json, not our setting
     resp1 = rpc_client.get_heartbeat()
-    assert resp1.behavior_tree_update[0].tree_id.name == "Eternal Guard"
+    assert resp1.behavior_tree_update[0].tree_id.name == "my_tree"
     wait_until(partial(assert_valid_tick_status_at_idx, rpc_client, 0))
 
     wait_until(partial(assert_test_status,
-                       rpc_client, "Eternal Guard", TreeStatus.TICKING))
+                       rpc_client, "my_tree", TreeStatus.TICKING))
 
 
 def test_load_interactive_tree(rpc_client: RPCClient):
@@ -82,7 +84,7 @@ def test_load_interactive_tree(rpc_client: RPCClient):
     resp0 = rpc_client.get_heartbeat()
     assert resp0.behavior_tree_update[0].tick_status == TickStatus.INVALID
     wait_until(partial(assert_test_status,
-                       rpc_client, "Eternal Guard", TreeStatus.WAITING_ACK))
+                       rpc_client, "my_tree", TreeStatus.WAITING_ACK))
 
     # tick tree
     rpc_client.tick_tree("my_tree")
@@ -90,10 +92,27 @@ def test_load_interactive_tree(rpc_client: RPCClient):
 
     # back to waiting for tick
     wait_until(partial(assert_test_status,
-                       rpc_client, "Eternal Guard", TreeStatus.WAITING_ACK))
+                       rpc_client, "my_tree", TreeStatus.WAITING_ACK))
 
 
 def test_pause_tree(rpc_client: RPCClient):
+    tree_path = Path(__file__).parent / "artifacts" / "eternal_guard.json"
+    rpc_client.load_new_tree(
+        new_tree_filepath=str(tree_path),
+        tick_config="CONTINUOUS",
+        tick_delay_ms=10,
+        tree_name="my_tree"
+    )
+
+    wait_until(partial(assert_test_status, rpc_client, "my_tree", TreeStatus.IDLE))
+    rpc_client.start_tree("my_tree")
+    wait_until(partial(assert_test_status, rpc_client,
+                       "my_tree", TreeStatus.TICKING))
+    rpc_client.pause_tree("my_tree")
+    wait_until(partial(assert_test_status, rpc_client, "my_tree", TreeStatus.IDLE))
+
+
+def test_tree_details(rpc_client: RPCClient):
     tree_path = Path(__file__).parent / "artifacts" / "eternal_guard.json"
     rpc_client.load_new_tree(
         new_tree_filepath=str(tree_path),
@@ -102,7 +121,47 @@ def test_pause_tree(rpc_client: RPCClient):
         tree_name="my_tree"
     )
 
-    wait_until(partial(assert_test_status, rpc_client, "Eternal Guard", TreeStatus.IDLE))
-
+    wait_until(partial(assert_test_status, rpc_client, "my_tree", TreeStatus.IDLE))
     rpc_client.start_tree("my_tree")
-    wait_until(partial(assert_test_status, rpc_client, "Eternal Guard", TreeStatus.TICKING))
+    # tick the tree to get some real statuses
+    rpc_client.tick_tree("my_tree")
+    wait_until(partial(assert_test_status, rpc_client,
+                       "my_tree", TreeStatus.WAITING_ACK))
+
+    details = rpc_client.get_detailed_update(tree_name="my_tree")
+    assert details.tree_id.name == "my_tree"
+    assert details.tree_status == TreeStatus.WAITING_ACK
+    assert details.node_info.id.name == "Eternal Guard"
+    assert len(details.node_info.children) == 3
+    assert details.node_info.children[0].id.name == "Condition 1"
+
+
+def test_uuid_matching(rpc_client: RPCClient):
+    tree_path = Path(__file__).parent / "artifacts" / "eternal_guard.json"
+    rpc_client.load_new_tree(
+        new_tree_filepath=str(tree_path),
+        tick_config="INTERACTIVE",
+        tick_delay_ms=10,
+        tree_name="eg"
+    )
+    rpc_client.tick_tree(tree_name="eg")
+
+    tree_path_2 = Path(__file__).parent / "artifacts" / "eggs.json"
+    rpc_client.load_new_tree(
+        new_tree_filepath=str(tree_path_2),
+        tick_config="INTERACTIVE",
+        tick_delay_ms=10,
+        tree_name="egg"
+    )
+    rpc_client.tick_tree(tree_name="egg")
+
+    wait_until(lambda: len(rpc_client.get_heartbeat().behavior_tree_update) == 2)
+    heartbeat = rpc_client.get_heartbeat()
+
+    for msg in heartbeat.behavior_tree_update:
+        print(msg.tree_id)
+        name_details = rpc_client.get_detailed_update(tree_name=msg.tree_id.name)
+        uuid_details = rpc_client.get_detailed_update(tree_uuid=msg.tree_id.uuid)
+        assert isinstance(name_details, TreeDetails)
+        assert isinstance(uuid_details, TreeDetails)
+        assert name_details == uuid_details
